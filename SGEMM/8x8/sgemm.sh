41c6acc8a4e0de06b81dddca7d299554b52eb2f1
Xiuxia Zhang <zhangxiuxia1@gmail.com>
2016-11-21 19:22:51 +0800

    Added files in 8x8 blocking

---
+++ SGEMM/8x8/sgemm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##compile code ###
+echo "...............compiling the assembly code .........."
+echo "....................................................."
+KeplerAs.pl -i sgemm_tn_192x192.sass sgemm_tn_192x192_template.cubin sgemm_tn_192x192.cubin
+echo "............... compile sgemm.cpp ..................."
+echo "....................................................."
+make
+
+file="ppopp_time.txt";
+echo "our SGEMM and cuBLAS SGEMM peformance is writen in $file"
+for ((i = 768; i <= 12288; i = i + i))
+do
+    echo "Matrix dimension: [M, K, N] " $i, $i, $i
+    ./sgemm $i $i $i >> $file
+    sleep 5
+done
