57232022aec5ce7db75729162aaeb01ecb54fa59
Xiuxia Zhang <zhangxiuxia1@gmail.com>
2016-12-05 13:08:26 +0800

    modify sgemm.sh

--- SGEMM/8x8/sgemm.sh
+++ SGEMM/8x8/sgemm.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-##compile code ###
-echo "...............compiling the assembly code .........."
-echo "....................................................."
-KeplerAs.pl -i sgemm_tn_192x192.sass sgemm_tn_192x192_template.cubin sgemm_tn_192x192.cubin
-echo "............... compile sgemm.cpp ..................."
-echo "....................................................."
-make
+echo "benchmarking our 8x8 blocking SGEMM and cuBLAS SGEMM ..."
 
 file="ppopp_time.txt";
 echo "our SGEMM and cuBLAS SGEMM peformance is writen in $file"
