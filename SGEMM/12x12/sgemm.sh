b93330fcbef9be0e0854dfc6f70f6cb7cf7f0f77
Xiuxia Zhang <zhangxiuxia1@gmail.com>
2016-12-05 13:06:38 +0800

    sgemm benchmarking script

--- SGEMM/12x12/sgemm.sh
+++ SGEMM/12x12/sgemm.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-##compile code ###
-echo "...............compiling the assembly code .........."
-echo "....................................................."
-KeplerAs.pl -i sgemm_tn_192x192.sass sgemm_tn_192x192_template.cubin sgemm_tn_192x192.cubin
-echo "............... compile sgemm.cpp ..................."
-echo "....................................................."
-make
-
+echo "Benchmarking SGEMM performance ....."
 file="ppopp_time.txt";
 echo "our SGEMM and cuBLAS SGEMM peformance is writen in $file"
 for ((i = 768; i <= 12288; i = i + i))
