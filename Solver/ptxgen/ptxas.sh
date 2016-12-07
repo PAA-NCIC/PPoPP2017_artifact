46a6d161c6c2f235ea03159ab3f1f82f4a85d623
Xiuxia Zhang <zhangxiuxia1@gmail.com>
2016-12-07 22:04:48 +0800

    change solver mode to SM_35 by default (ptxgen)

--- Solver/ptxgen/ptxas.sh
+++ Solver/ptxgen/ptxas.sh
@@ -5,5 +5,5 @@ do
      f=`echo $p | cut -d . -f 1 |cut -d / -f 2`
      fout="cubin/"$f".cubin"
      echo $fout
-     ptxas -arch sm_21 -m 64 $p -o $fout
+     ptxas -arch sm_35 -m 64 $p -o $fout
 done
