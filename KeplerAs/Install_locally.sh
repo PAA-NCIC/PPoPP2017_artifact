96c43f1b891f56570caa084e4a63eba0ded381ac
Xiuxia Zhang <zhangxiuxia1@gmail.com>
2016-12-09 01:56:30 +0800

    Added install locally script

---
+++ KeplerAs/Install_locally.sh
@@ -0,0 +1,6 @@
+perl Makefile.PL
+make
+
+#configure
+#export PERL5LIB=/home/xiuxia/PP2017_artifact/KeplerAs/blib/lib/:$PERL5LIB
+#export PATH=/home/xiuxia/PPoPP2017_artifact/KeplerAs/blib/script:$PATH
